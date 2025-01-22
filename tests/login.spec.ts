e63b6f314f8fe054ae9f495bef131e3ce173b956
Maicon Hatada <maicon.hatada17@gmail.com git config --global user.name Maicon>
2025-01-21 22:15:17 -0300

    Alterações no projeto

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,28 +1,23 @@
 import { test, expect } from '@playwright/test';
+import { BASE_URL } from '../config';
 
-test('entrar na pagina Mark L', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
 
+test.beforeEach(async ({ page }) => {
+  await page.goto(BASE_URL);
+});
+test('1.entrar na pagina Mark L', async ({ page }) => {
   const inputTaskName = page.locator('#newTask')  // definição de um objeto
   await inputTaskName.fill('conseguir usar o playwright') // step promessa de q irá fazer algo
   await page.click('button[class*=ButtonNewTask]');
     
-  const taskText = page.locator('text=conseguir usar o playwright');
+  const taskText = page.locator('text=conseguir usar o playwright'); // definição de um objeto
   await expect(taskText).toBeVisible();
 
-  await page.waitForTimeout(200)
     
-  
-
-
-  // Expect a title "to contain" a substring.
- // await expect(page).toHaveTitle(/Gerencie suas tarefas com Mark L/);
 });
 
-test('deve apagar o registro', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-
-  const deleteButton = page.locator('button[class="_listItemDeleteButton_1kgm5_52"]');
+test('2.deve apagar o registro', async ({ page }) => {
+  const deleteButton = page.locator('button[class="_listItemDeleteButton_1kgm5_52"]'); // definição de um objeto
   await deleteButton.click();
 
 });
